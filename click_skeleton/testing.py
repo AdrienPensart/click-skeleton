f0a326bfb5580f9bf71b318906ff968b228f3de6
Adrien Pensart <crunchengine@gmail.com>
2020-09-25 12:08:45 -0400

    catch_exceptions false by default and propagate kwargs

--- click_skeleton/testing.py
+++ click_skeleton/testing.py
@@ -3,15 +3,20 @@ import logging
 import traceback
 from typing import Any
 import click
-from click.testing import Result, CliRunner
+from click import testing
 from click_skeleton import helpers
 
 logger = logging.getLogger(__name__)
 
 
-def run_cli(cli_runner: CliRunner, called_cli: click.core.Group, *args: Any) -> str:
+def run_cli(cli_runner: testing.CliRunner, called_cli: click.core.Group, *args: Any, catch_exceptions: bool = False, **kwargs: Any) -> str:
     '''Helper to run the CLI for pytest-click unit tests'''
-    result: Result = cli_runner.invoke(called_cli, *args)
+    result: testing.Result = cli_runner.invoke(
+        called_cli,
+        *args,
+        catch_exceptions=catch_exceptions,
+        **kwargs,
+    )  # type: ignore
     logger.debug(result.output)
     if result.exception:
         exc_info = result.exc_info
@@ -19,6 +24,6 @@ def run_cli(cli_runner: CliRunner, called_cli: click.core.Group, *args: Any) -> 
     if result.exit_code != 0:
         elems = helpers.recursive_str(*args)
         elems = ' '.join(elems)
-        logger.error(f'Failed : {cli_runner.get_default_prog_name(called_cli)} {elems}')
+        logger.error(f'Failed : {cli_runner.get_default_prog_name(called_cli)} {elems}')  # pylint:disable=logging-fstring-interpolation
     assert result.exit_code == 0
     return result.output.rstrip()
